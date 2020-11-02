9533b876f92dc44bc12f0746a96d6ae0de59c2b0
konan.higo <conan.hg1550@gmail.com>
2020-11-02 18:35:01 +0900

    Add LoginFilter

--- src/filters/LoginFilter.java
+++ src/filters/LoginFilter.java
@@ -1,6 +1,7 @@
 package filters;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -8,6 +9,11 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import models.Employee;
 
 /**
  * Servlet Filter implementation class LoginFilter
@@ -16,35 +22,52 @@ import javax.servlet.annotation.WebFilter;
 public class LoginFilter implements Filter {
 
     /**
-     * Default constructor. 
+     * Default constructor.
      */
     public LoginFilter() {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        // place your code here
 
-		// pass the request along the filter chain
-		chain.doFilter(request, response);
-	}
+        // pass the request along the filter chain
+        String context_path = ((HttpServletRequest)request).getContextPath();
+        String servlet_path = ((HttpServletRequest)request).getServletPath();
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
-	}
+        if(!servlet_path.matches("/css.*")) {
+            HttpSession session =((HttpServletRequest)request).getSession();
+
+            Employee e = (Employee)session.getAttribute("login_employee");
+
+            if(!servlet_path.equals("/login")) {
+
+                if(e == null){
+                    ((HttpServletResponse)response).sendRedirect(context_path + "/login");
+                    return;
+                }
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+        // TODO Auto-generated method stub
+    }
 
 }
